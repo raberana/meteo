1599eb514a7a486c0ba3a64f151539605d74cb48
Roj Beraña <raberana@gmail.com>
2021-06-19 13:46:47 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meteo-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent
+{
+    title = 'meteo';
+}
